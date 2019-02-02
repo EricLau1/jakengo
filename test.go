13f786683e97e600e2566913a96ae6ddf6798e7a
EricLau1 <ericlau.oliveira@gmail.com>
2019-02-02 20:28:03 -0200

    commit

--- test.go
+++ test.go
@@ -4,53 +4,47 @@ import(
   "fmt"
   "log"
 )
-func main(){
-  TestChooseStone()
-  TestChoosePaper()
-  TestChooseScissor()
-  TestWrongChoice()
-}
 
 func TestChooseStone() {
-  p := jankengo.Player{"Human", jankengo.STONE,}
-  winner, err := jankengo.Pow(p)
+  p := Player{"Human", STONE,}
+  winner, err := Pow(p)
   if err != nil {
     log.Fatal(err)
   }
-  fmt.Printf("Winner: %s, Choose: %s\n", winner.Name, jankengo.Itens[winner.Choose])
+  fmt.Printf("Winner: %s, Choose: %s\n", winner.Name, Itens[winner.Choose])
 }
 
 func TestChoosePaper() {
-  p := jankengo.Player{"Human", jankengo.PAPER,}
-  winner, err := jankengo.Pow(p)
+  p := Player{"Human", PAPER,}
+  winner, err := Pow(p)
   if err != nil {
     log.Fatal(err)
   }
   fmt.Println("************************************")
-  fmt.Printf("Winner: %s, Choose: %s\n", winner.Name, jankengo.Itens[winner.Choose])
+  fmt.Printf("Winner: %s, Choose: %s\n", winner.Name,  Itens[winner.Choose])
   fmt.Println("************************************")
 }
 
 func TestChooseScissor() {
-  p := jankengo.Player{"Human", jankengo.SCISSOR,}
-  winner, err := jankengo.Pow(p)
+  p :=  Player{"Human",  SCISSOR,}
+  winner, err :=  Pow(p)
   if err != nil {
     log.Fatal(err)
   }
   fmt.Println("************************************")
-  fmt.Printf("Winner: %s, Choose: %s\n", winner.Name, jankengo.Itens[winner.Choose])
+  fmt.Printf("Winner: %s, Choose: %s\n", winner.Name,  Itens[winner.Choose])
   fmt.Println("************************************")
 }
 
 func TestWrongChoice() {
-  p := jankengo.Player{"Human", 3,} 
-  _, err := jankengo.Pow(p)
-  if err == jankengo.ErrWrongChoice {
+  p :=  Player{"Human", 3,} 
+  _, err :=  Pow(p)
+  if err ==  ErrWrongChoice {
     log.Println(err)
   }
   p.Choose = -1
-  _, err = jankengo.Pow(p)
-  if err == jankengo.ErrWrongChoice {
+  _, err =  Pow(p)
+  if err ==  ErrWrongChoice {
     log.Println(err)
   }
 }
